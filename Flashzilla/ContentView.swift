bc464008699e25d1eed5ec2a3a42a27905d4ddae
Milo Wyner <milowyner@gmail.com>
2021-09-22 13:38:09 -0700

    Example 1: Rotation gesture

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount: Angle = .degrees(0)
+    @State private var finalAmount: Angle = .degrees(0)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, World!")
+            .rotationEffect(currentAmount + finalAmount)
+            .gesture(
+                RotationGesture()
+                    .onChanged { angle in
+                        self.currentAmount = angle
+                    }
+                    .onEnded { angle in
+                        self.finalAmount += self.currentAmount
+                        self.currentAmount = .degrees(0)
+                    }
+            )
     }
 }
 
