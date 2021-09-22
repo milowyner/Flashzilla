128fbb16d94b4e5261059745d26be322638ccdf9
Milo Wyner <milowyner@gmail.com>
2021-09-22 12:50:18 -0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Milo Wyner on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
