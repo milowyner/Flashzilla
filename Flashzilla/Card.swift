5ce539961a53c4e85c654d1e71255636741c9ffe
Milo Wyner <milowyner@gmail.com>
2021-09-24 19:22:35 -0700

    Create CardView that shows the prompt and answer from a Card struct and shows/hides the answer when tapped

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Milo Wyner on 9/24/21.
+//
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
